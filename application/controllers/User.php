e1444ebd3ca28e07693c5fe908aef91a791262d5
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-11-20 05:44:13 +0700

    Membuat page user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,6 +6,10 @@ class User extends CI_Controller
     public function index()
     {
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        echo 'Selamat datang ' . $data['user']['name'];
+
+        $data['title'] =  'My Profile';
+        // $this->load->view('templates/auth_header', $data);
+        $this->load->view('user/index', $data);
+        // $this->load->view('templates/auth_footer');
     }
 }
