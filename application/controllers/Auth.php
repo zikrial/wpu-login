1c27d7858a0f86afa054f3440d69e7397fb691da
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-11-19 18:42:30 +0700

    Menambah database dan memasukan inputan dari registration ke tabel user

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -20,7 +20,16 @@ class Auth extends CI_Controller
     public function registration()
     {
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has already registered!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[6]|matches[password2]', [
+            'matches' => 'password dont match!',
+            'min_length' => 'password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]', [
+            'matches' => 'password dont match!',
+        ]);
 
         if ($this->form_validation->run() == false) {
             $data['title'] =  'WPU User Registration';
@@ -28,7 +37,19 @@ class Auth extends CI_Controller
             $this->load->view('auth/registration');
             $this->load->view('templates/auth_footer');
         } else {
-            echo 'data berhasil ditambahkan';
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation your account has been created, Please login</div>');
+            redirect('auth');
         }
     }
 }
