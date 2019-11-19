8511d8dcd37b1afb8d964493aa5479d2c780690b
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-11-19 18:05:34 +0700

    Menambahkan rules pada data pada registration

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,18 +3,32 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
 
     public function index()
     {
-        $this->load->view('templates/auth_header');
+        $data['title'] =  'WPU User Login';
+        $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
 
     public function registration()
     {
-        $this->load->view('templates/auth_header');
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] =  'WPU User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            echo 'data berhasil ditambahkan';
+        }
     }
 }
