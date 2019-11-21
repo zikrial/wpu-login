26875e07bdd3a77cfc56202258f553f5d72d2e3d
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-11-21 07:27:20 +0700

    Membuat fungsi untuk membuat menu dan sub menu

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -42,32 +42,36 @@
                     ?>
 
                 <?php foreach ($subMenu as $sm) : ?>
-                    <li class="nav-item">
+                    <?php if ($title == $sm['title']) : ?>
+                        <li class="nav-item active">
+                        <?php else : ?>
+                        <li class="nav-item">
+                        <?php endif; ?>
                         <a class="nav-link" href="<?= base_url($sm['url']); ?>">
                             <i class="<?= $sm['icon']; ?>"></i>
                             <span><?= $sm['title']; ?></span></a>
-                    </li>
+                        </li>
+
+                    <?php endforeach; ?>
+
+                    <hr class="sidebar-divider">
 
                 <?php endforeach; ?>
 
-                <hr class="sidebar-divider">
+                <!-- Nav Item - Logout -->
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('auth/logout'); ?>" data-toggle="modal" data-target="#logoutModal">
+                        <i class="fas fa-fw fa-sign-out-alt"></i>
+                        <span>Logout</span></a>
+                </li>
 
-            <?php endforeach; ?>
+                <!-- Divider -->
+                <hr class="sidebar-divider d-none d-md-block">
 
-            <!-- Nav Item - Logout -->
-            <li class="nav-item">
-                <a class="nav-link" href="<?= base_url('auth/logout'); ?>" data-toggle="modal" data-target="#logoutModal">
-                    <i class="fas fa-fw fa-sign-out-alt"></i>
-                    <span>Logout</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
+                <!-- Sidebar Toggler (Sidebar) -->
+                <div class="text-center d-none d-md-inline">
+                    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+                </div>
 
         </ul>
         <!-- End of Sidebar -->
